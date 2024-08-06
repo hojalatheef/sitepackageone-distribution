2b76d70dd635684019e5d7f138af8d49dc6a058f
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-08-06 14:47:42 +0200

    [TASK] Installation and basic configuration of extension

---
+++ Source/Extension/sitepackageone/ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Site package for sitepackageone distribution',
+    'description' => '',
+    'category' => 'templates',
+    'author' => 'Hoja Mustaffa Abdul Latheef',
+    'author_email' => 'extensions@jweiland.net, extensions@jweiland.net',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.2.0-13.2.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
