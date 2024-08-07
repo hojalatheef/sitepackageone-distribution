eafb47ecd00084645b2b9f52ab3997ef4501285a
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-08-07 16:18:24 +0200

    [TASK] Added new page type for section

--- Source/Extension/sitepackageone/ext_tables.php
+++ Source/Extension/sitepackageone/ext_tables.php
@@ -3,3 +3,21 @@
 if (!defined('TYPO3')) {
     die('Access denied.');
 }
+
+use TYPO3\CMS\Core\DataHandling\PageDoktypeRegistry;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+call_user_func(
+    static function () {
+        // Register new page types for section
+        $sectionDocType = 369;
+        $dokTypeRegistry = GeneralUtility::makeInstance(PageDoktypeRegistry::class);
+        $dokTypeRegistry->add(
+            $sectionDocType,
+            [
+                'type' => 'web',
+                'allowedTables' => '*',
+            ]
+        );
+    }
+);
